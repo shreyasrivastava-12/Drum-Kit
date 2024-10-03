3ee8d231341df538d516d6790d44d660bc8a557a
shreyasrivastava-12 <shreyasri1203@gmail.com>
2024-10-03 18:58:17 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -2,45 +2,54 @@ var drums = document.querySelectorAll(".drum");
 
 for(var i = 0;i<drums.length;i++){
     drums[i].addEventListener("click", function(){
-        var textInside = this.textContent;
-        var instrument;
-
-        switch(textInside){
-            case "w":
-                instrument = "tom-1";
-                break;
-
-            case "a":
-                instrument = "tom-2";
-                break;
-
-            case "s":
-                instrument = "tom-3";
-                break;
-
-            case "d":
-                instrument = "tom-4";
-                break;
-
-            case "j":
-                instrument = "snare";
-                break;
-
-            case "k":
-                instrument = "crash";
-                break;
-
-            case "l":
-                instrument = "kick-bass";
-                break;
-
-            default:
-                console.log("textInside");
-                return;
-        }
-
-        var aud = "./sounds/" + instrument + ".mp3";
-        var audio = new Audio(aud);
-        audio.play();
+        var char = this.textContent;
+        makeSound(char);
     });
+}
+
+document.addEventListener("keydown", function(event){
+    var char = event.key;
+    makeSound(char);
+});
+
+function makeSound(char){
+    var instrument;
+
+    switch(char){
+        case "w":
+            instrument = "tom-1";
+            break;
+
+        case "a":
+            instrument = "tom-2";
+            break;
+
+        case "s":
+            instrument = "tom-3";
+            break;
+
+        case "d":
+            instrument = "tom-4";
+            break;
+
+        case "j":
+            instrument = "snare";
+            break;
+
+        case "k":
+            instrument = "crash";
+            break;
+
+        case "l":
+            instrument = "kick-bass";
+            break;
+
+        default:
+            console.log("textInside");
+            return;
+    }
+
+    var aud = "./sounds/" + instrument + ".mp3";
+    var audio = new Audio(aud);
+    audio.play();
 }
